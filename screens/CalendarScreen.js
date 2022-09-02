56317546f95dc2e4fb1be3448cf2ce5b87017950
Skylar Foster <vskylar.foster@gmail.com>
2022-09-02 14:32:18 -0500

    CalendarScreen

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import { CalendarList, Agenda} from 'react-native-calendars';
 
 
 
 const CalendarScreen = () => {
-
+    const [agenda, setAgenda] = useState([]);
+    
+    return (
+        <View>
+            <View>
+                <CalendarList
+                    style={styles.calendar}
+                    horizontal={true}
+                    pagingEnabled={true}
+                    pastScrollRange={4}
+                    futureScrollRange={12}
+                    scrollEnabled={true}
+                />
+            </View>
+            <View>
+                <Agenda 
+                    items={{agenda}}
+                />
+            </View>
+        </View>
+    );
 };
 
 
 const styles = StyleSheet.create ({
-
+    calendar:{
+        backgroundColor:'#D9F2D2'
+    }
 })
 
 export default CalendarScreen;
